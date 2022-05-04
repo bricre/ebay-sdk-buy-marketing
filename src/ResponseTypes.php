cf6a4f3f1a635f165331869f1781ba15f7c83c4e
Allan Sun <allan.sun@bricre.com>
2022-05-04 16:54:03 +0100

    Generated against OpenAPI on 2022-05-04T15:54:03+00:00

--- src/ResponseTypes.php
+++ src/ResponseTypes.php
@@ -7,56 +7,145 @@ use OpenAPI\Runtime\ResponseTypes as AbstractResponseTypes;
 class ResponseTypes extends AbstractResponseTypes
 {
     public array $types = [
-        'fetchItemAspects' => [
-            '200.' => 'Ebay\\Commerce\\Taxonomy\\Model\\GetCategoriesAspectResponse',
+        'search' => [
+            '200.' => 'Ebay\\Buy\\Browse\\Model\\SearchPagedCollection',
             '400.' => 'OpenAPI\\Runtime\\GenericResponse',
-            '404.' => 'OpenAPI\\Runtime\\GenericResponse',
+            '409.' => 'OpenAPI\\Runtime\\GenericResponse',
             '500.' => 'OpenAPI\\Runtime\\GenericResponse',
         ],
-        'getDefaultCategoryTreeId' => [
-            '200.' => 'Ebay\\Commerce\\Taxonomy\\Model\\BaseCategoryTree',
+        'searchByImage' => [
+            '200.' => 'Ebay\\Buy\\Browse\\Model\\SearchPagedCollection',
+            '400.' => 'OpenAPI\\Runtime\\GenericResponse',
+            '409.' => 'OpenAPI\\Runtime\\GenericResponse',
+            '500.' => 'OpenAPI\\Runtime\\GenericResponse',
+        ],
+        'getItem' => [
+            '200.' => 'Ebay\\Buy\\Browse\\Model\\Item',
+            '400.' => 'OpenAPI\\Runtime\\GenericResponse',
+            '404.' => 'OpenAPI\\Runtime\\GenericResponse',
+            '409.' => 'OpenAPI\\Runtime\\GenericResponse',
+            '500.' => 'OpenAPI\\Runtime\\GenericResponse',
+        ],
+        'getItemByLegacyId' => [
+            '200.' => 'Ebay\\Buy\\Browse\\Model\\Item',
+            '400.' => 'OpenAPI\\Runtime\\GenericResponse',
+            '404.' => 'OpenAPI\\Runtime\\GenericResponse',
+            '409.' => 'OpenAPI\\Runtime\\GenericResponse',
+            '500.' => 'OpenAPI\\Runtime\\GenericResponse',
+        ],
+        'getItems' => [
+            '200.' => 'Ebay\\Buy\\Browse\\Model\\Items',
+            '400.' => 'OpenAPI\\Runtime\\GenericResponse',
+            '404.' => 'OpenAPI\\Runtime\\GenericResponse',
+            '409.' => 'OpenAPI\\Runtime\\GenericResponse',
+            '500.' => 'OpenAPI\\Runtime\\GenericResponse',
+        ],
+        'getItemsByItemGroup' => [
+            '200.' => 'Ebay\\Buy\\Browse\\Model\\ItemGroup',
+            '400.' => 'OpenAPI\\Runtime\\GenericResponse',
+            '404.' => 'OpenAPI\\Runtime\\GenericResponse',
+            '409.' => 'OpenAPI\\Runtime\\GenericResponse',
+            '500.' => 'OpenAPI\\Runtime\\GenericResponse',
+        ],
+        'checkCompatibility' => [
+            '200.' => 'Ebay\\Buy\\Browse\\Model\\CompatibilityResponse',
+            '400.' => 'OpenAPI\\Runtime\\GenericResponse',
+            '404.' => 'OpenAPI\\Runtime\\GenericResponse',
+            '409.' => 'OpenAPI\\Runtime\\GenericResponse',
+            '500.' => 'OpenAPI\\Runtime\\GenericResponse',
+        ],
+        'addItem' => [
+            '200.' => 'Ebay\\Buy\\Browse\\Model\\RemoteShopcartResponse',
             '204.' => 'OpenAPI\\Runtime\\GenericResponse',
             '400.' => 'OpenAPI\\Runtime\\GenericResponse',
+            '403.' => 'OpenAPI\\Runtime\\GenericResponse',
+            '409.' => 'OpenAPI\\Runtime\\GenericResponse',
             '500.' => 'OpenAPI\\Runtime\\GenericResponse',
         ],
-        'getCategoryTree' => [
-            '200.' => 'Ebay\\Commerce\\Taxonomy\\Model\\CategoryTree',
-            '400.' => 'OpenAPI\\Runtime\\GenericResponse',
-            '404.' => 'OpenAPI\\Runtime\\GenericResponse',
-            '500.' => 'OpenAPI\\Runtime\\GenericResponse',
-        ],
-        'getCategorySubtree' => [
-            '200.' => 'Ebay\\Commerce\\Taxonomy\\Model\\CategorySubtree',
-            '400.' => 'OpenAPI\\Runtime\\GenericResponse',
-            '404.' => 'OpenAPI\\Runtime\\GenericResponse',
-            '500.' => 'OpenAPI\\Runtime\\GenericResponse',
-        ],
-        'getCategorySuggestions' => [
-            '200.' => 'Ebay\\Commerce\\Taxonomy\\Model\\CategorySuggestionResponse',
+        'getShoppingCart' => [
+            '200.' => 'Ebay\\Buy\\Browse\\Model\\RemoteShopcartResponse',
             '204.' => 'OpenAPI\\Runtime\\GenericResponse',
             '400.' => 'OpenAPI\\Runtime\\GenericResponse',
+            '403.' => 'OpenAPI\\Runtime\\GenericResponse',
             '404.' => 'OpenAPI\\Runtime\\GenericResponse',
             '500.' => 'OpenAPI\\Runtime\\GenericResponse',
         ],
-        'getItemAspectsForCategory' => [
-            '200.' => 'Ebay\\Commerce\\Taxonomy\\Model\\AspectMetadata',
+        'removeItem' => [
+            '200.' => 'Ebay\\Buy\\Browse\\Model\\RemoteShopcartResponse',
             '204.' => 'OpenAPI\\Runtime\\GenericResponse',
+            '400.' => 'OpenAPI\\Runtime\\GenericResponse',
+            '403.' => 'OpenAPI\\Runtime\\GenericResponse',
+            '409.' => 'OpenAPI\\Runtime\\GenericResponse',
+            '500.' => 'OpenAPI\\Runtime\\GenericResponse',
+        ],
+        'updateQuantity' => [
+            '200.' => 'Ebay\\Buy\\Browse\\Model\\RemoteShopcartResponse',
+            '400.' => 'OpenAPI\\Runtime\\GenericResponse',
+            '403.' => 'OpenAPI\\Runtime\\GenericResponse',
+            '409.' => 'OpenAPI\\Runtime\\GenericResponse',
+            '500.' => 'OpenAPI\\Runtime\\GenericResponse',
+        ],
+        'getDealItems' => [
+            '200.' => 'Ebay\\Buy\\Deal\\Model\\DealItemSearchResponse',
+            '400.' => 'OpenAPI\\Runtime\\GenericResponse',
+            '403.' => 'OpenAPI\\Runtime\\GenericResponse',
+            '500.' => 'OpenAPI\\Runtime\\GenericResponse',
+        ],
+        'getEvent' => [
+            '200.' => 'Ebay\\Buy\\Deal\\Model\\Event',
+            '400.' => 'OpenAPI\\Runtime\\GenericResponse',
+            '403.' => 'OpenAPI\\Runtime\\GenericResponse',
+            '404.' => 'OpenAPI\\Runtime\\GenericResponse',
+            '500.' => 'OpenAPI\\Runtime\\GenericResponse',
+        ],
+        'getEvents' => [
+            '200.' => 'Ebay\\Buy\\Deal\\Model\\EventSearchResponse',
+            '400.' => 'OpenAPI\\Runtime\\GenericResponse',
+            '403.' => 'OpenAPI\\Runtime\\GenericResponse',
+            '500.' => 'OpenAPI\\Runtime\\GenericResponse',
+        ],
+        'getEventItems' => [
+            '200.' => 'Ebay\\Buy\\Deal\\Model\\EventItemSearchResponse',
+            '400.' => 'OpenAPI\\Runtime\\GenericResponse',
+            '403.' => 'OpenAPI\\Runtime\\GenericResponse',
+            '500.' => 'OpenAPI\\Runtime\\GenericResponse',
+        ],
+        'getAccess' => [
+            '200.' => 'Ebay\\Buy\\Feed\\Model\\ApplicationAccess',
+            '204.' => 'OpenAPI\\Runtime\\GenericResponse',
+            '500.' => 'OpenAPI\\Runtime\\GenericResponse',
+        ],
+        'getFeedType' => [
+            '200.' => 'Ebay\\Buy\\Feed\\Model\\FeedType',
             '400.' => 'OpenAPI\\Runtime\\GenericResponse',
             '404.' => 'OpenAPI\\Runtime\\GenericResponse',
             '500.' => 'OpenAPI\\Runtime\\GenericResponse',
         ],
-        'getCompatibilityProperties' => [
-            '200.' => 'Ebay\\Commerce\\Taxonomy\\Model\\GetCompatibilityMetadataResponse',
-            '204.' => 'OpenAPI\\Runtime\\GenericResponse',
+        'getFeedTypes' => [
+            '200.' => 'Ebay\\Buy\\Feed\\Model\\FeedTypeSearchResponse',
             '400.' => 'OpenAPI\\Runtime\\GenericResponse',
+            '500.' => 'OpenAPI\\Runtime\\GenericResponse',
+        ],
+        'downloadFile' => [
+            '200.' => 'OpenAPI\\Runtime\\GenericResponse',
+            '206.' => 'OpenAPI\\Runtime\\GenericResponse',
+            '400.' => 'OpenAPI\\Runtime\\GenericResponse',
+            '403.' => 'OpenAPI\\Runtime\\GenericResponse',
+            '404.' => 'OpenAPI\\Runtime\\GenericResponse',
+            '416.' => 'OpenAPI\\Runtime\\GenericResponse',
+            '500.' => 'OpenAPI\\Runtime\\GenericResponse',
+        ],
+        'getFile' => [
+            '200.' => 'Ebay\\Buy\\Feed\\Model\\FileMetadata',
+            '400.' => 'OpenAPI\\Runtime\\GenericResponse',
+            '403.' => 'OpenAPI\\Runtime\\GenericResponse',
             '404.' => 'OpenAPI\\Runtime\\GenericResponse',
             '500.' => 'OpenAPI\\Runtime\\GenericResponse',
         ],
-        'getCompatibilityPropertyValues' => [
-            '200.' => 'Ebay\\Commerce\\Taxonomy\\Model\\GetCompatibilityPropertyValuesResponse',
-            '204.' => 'OpenAPI\\Runtime\\GenericResponse',
+        'getFiles' => [
+            '200.' => 'Ebay\\Buy\\Feed\\Model\\FileMetadataSearchResponse',
             '400.' => 'OpenAPI\\Runtime\\GenericResponse',
-            '404.' => 'OpenAPI\\Runtime\\GenericResponse',
+            '403.' => 'OpenAPI\\Runtime\\GenericResponse',
             '500.' => 'OpenAPI\\Runtime\\GenericResponse',
         ],
         'getMerchandisedProducts' => [
